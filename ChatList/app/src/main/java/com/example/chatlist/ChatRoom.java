b6d85e73de84d2269cef615aaa55b167381240b8
rita224 <apple450396@gmail.com>
2019-09-02 11:31:19 +0800

    push

---
+++ ChatList/app/src/main/java/com/example/chatlist/ChatRoom.java
@@ -0,0 +1,21 @@
+package com.example.chatlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.os.Bundle;
+
+public class ChatRoom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_room);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+}
